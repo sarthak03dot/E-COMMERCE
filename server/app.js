8fb8865a9abf2c59a62b913b98eca10f75c7b53b
Sarthak Singh <137082483+sarthak03dot@users.noreply.github.com>
2025-08-14 20:52:44 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -15,15 +15,10 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "https://bd-ai-gpgoc.onrender.com",
+
     methods: ["GET", "POST", "PUT", "DELETE"],
+
     credentials: true,
   })
 );
